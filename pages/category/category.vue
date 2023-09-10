6b66fcca7b2291a7a985d4877dc407b889c68fc0
truthhun <1272881215@qq.com>
2023-09-10 17:28:09 +0800

    资料页、列表页

--- pages/category/category.vue
+++ pages/category/category.vue
@@ -1,22 +1,18 @@
 <template>
 	<view>
-		
+		<filterDocument :args="args" :base-height="30" />
 	</view>
 </template>
-
 <script>
+	import filterDocument from '@/compomnents/filterDocument.vue'
 	export default {
+		components: {
+			filterDocument
+		},
 		data() {
 			return {
-				
+				args: {}
 			}
 		},
-		methods: {
-			
-		}
 	}
 </script>
-
-<style>
-
-</style>
